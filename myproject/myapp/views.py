da4e4ff50f45b897d61e7c8d611628ef71b9feef
Binish Moosa <150269880+Binish-1208@users.noreply.github.com>
2026-08-24 00:37:10 +0530

    Define View and configure URLs

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    context = {
+        'title': 'Full Stack Development Lab',
+        'heading': 'Implementing Django Views and Templates',
+        'name': 'Binish',
+        'is_logged_in': True,
+        'subjects': [
+            'Python',
+            'Django',
+            'HTML',
+            'CSS'
+        ]
+    }
+
+    return render(request, 'myapp/home.html', context)
